0c775f5cb137455a7905080e3aea40380154de02
gabrielrowan <gabrielrowan82@gmail.com>
2026-08-23 19:52:51 +0100

    added fill in star to tadas

--- src/components/TadaCard.tsx
+++ src/components/TadaCard.tsx
@@ -8,8 +8,8 @@ export function TadaCard({ text }: TadaCardProps) {
     return (
         <div className="relative group">
             <div className="absolute -inset-1 bg-gradient-to-r from-indigo-300 to-fuchsia-300 rounded-lg blur opacity-60"></div>
-            <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-start justify-start space-x-6">
-                <Star className="w-6 h-6 text-indigo-300 flex-shrink-0" aria-hidden="true" />
+            <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-center justify-start space-x-6">
+                <Star className="w-6 h-6 text-indigo-300 flex-shrink-0 fill-indigo-200" aria-hidden="true" />
                 <p className="text-slate-800 text-base">{text}</p>
             </div>
         </div>
